8e1eb2a5664f7fcc6588d2286f6e8e02227c0588
Matt Davidson <davidso1@rose-hulman.edu>
2017-08-25 10:53:32 -0400

    bump version
    release 2017082500

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017080300;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017082500;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release   = '1';
 $plugin->requires  = 2015111600;        // Requires this Moodle version.
 $plugin->component = 'atto_sketch';
