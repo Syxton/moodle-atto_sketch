0d88fea18a8c79d3de40170ddcbb268281b8ae0f
Matt Davidson <davidso1@rose-hulman.edu>
2018-05-10 12:21:58 -0400

    Add GDPR privacy files

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -13,19 +13,32 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
- * Strings for component 'atto_sketch', language 'en'.
+ * Privacy Subsystem implementation for atto_sketch.
  *
  * @package    atto_sketch
- * @copyright  2013 Damyon Wiese  <damyon@moodle.com>
+ * @copyright  2018 Matt Davidson <davidso1@rose-hulman.edu>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Sketch';
-$string['privacy:metadata'] = 'The Sketch plugin does not save or export user data.';
-$string['sketchtitle'] = 'Sketch and Save';
-$string['insert'] = 'Insert Sketch';
-$string['settings'] = 'Sketch settings';
-$string['storeinrepo'] = 'Store as image file';
-$string['storeinrepo_desc'] = 'If checked the sketch will be saved as a standard image upload instead of a base64 inline image.';
+namespace atto_sketch\privacy;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Privacy Subsystem for atto_sketch implementing null_provider.
+ *
+ * @copyright  2018 Matt Davidson <davidso1@rose-hulman.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+}
